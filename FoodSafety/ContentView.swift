31098e72537e64e6c1698f13c098c4c29bc9a2aa
S Yi <syi123@gmail.com>
2021-08-23 21:35:27 -0400

    Initial Commit

---
+++ FoodSafety/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodSafety
+//
+//  Created by S Yi on 8/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
